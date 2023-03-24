6a959c3281bcd538a9faf1c130c666ca9d913f93
Lam1109 <1031941624@qq.com>
2023-03-24 22:43:04 +0800

    init

---
+++ src/test/java/com/lam/jingxi/JingxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lam.jingxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
